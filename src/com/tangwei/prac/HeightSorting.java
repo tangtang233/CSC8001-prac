6250db725768bbd7d5f35d6222d1da77d411f6e0
tangwei <toby911115@163.com>
2026-07-13 15:20:46 +0800

    Add height sorting practice stub

---
+++ src/com/tangwei/prac/HeightSorting.java
@@ -0,0 +1,21 @@
+package com.tangwei.prac;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 高矮排序
+ *
+ * @author TangWei on 2022/5/27
+ */
+public class HeightSorting {
+
+    public static List queueUp(List<Integer> raw) {
+        List<Integer> result = new ArrayList<>();
+
+        int current = 0;
+
+
+        return result;
+    }
+}
